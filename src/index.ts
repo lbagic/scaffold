53e4eb99a0a8ba35dc37c6bc36d8e733b1a7a582
Luka Bagic <luka.bagi@gmail.com>
2023-08-04 22:07:37 +0200

    Add nestjs

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const prompt = prompts({
   choices: [
     choice("Vite", "npm create vite@latest"),
     choice("Next", "npx create-next-app@latest"),
+    choice("Nest", "npx @nestjs/cli@latest new"),
     choice("Astro", "npm create astro@latest"),
     choice("ESLint", "npm init @eslint/config"),
     choice("Open Web Component", "npm init @open-wc"),
