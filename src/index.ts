da0d56c20e6ae46537dc158f808408997bcd29ea
Luka Bagic <luka.bagi@gmail.com>
2023-08-18 05:47:24 +0200

    fix: add missing shebang

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
+#!/usr/bin/env node
 import { commandChoice, prompt, run } from "./utils";
 
 const main = prompt({
   message: "What would you like to scaffold today?",
   choices: [
-    commandChoice("NestJS", "npm i -g @nestjs/cli; nest new"),
     commandChoice("Vite", "npm create vite@latest"),
     commandChoice("Next", "npx create-next-app@latest"),
-    // commandChoice("NestJS", "npx @nestjs/cli@latest new"),
+    commandChoice("NestJS", "npm i -g @nestjs/cli; nest new"),
     commandChoice("Astro", "npm create astro@latest"),
     commandChoice("ESLint", "npm init @eslint/config"),
     commandChoice("Open Web Component", "npm init @open-wc"),
