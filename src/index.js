04a1c25d38cf48c6a3f6d42e90170d50977507a8
Luka Bagic <luka.bagi@gmail.com>
2024-12-16 03:10:16 +0100

    refactor: cleanup

--- src/index.js
+++ src/index.js
@@ -1,70 +1,70 @@
 #!/usr/bin/env node
-import prompts from "prompts";
-import { reactJsTemplate } from "./template-react-js.js";
-import { exec } from "./utils.js";
+import prompts from 'prompts';
+import { reactJsTemplate } from './template-react-js.js';
+import { exec } from './utils.js';
 
 const cfg = {
-  name: "choice",
-  type: "select",
-  message: "What would you like to scaffold today?",
+  name: 'choice',
+  type: 'select',
+  message: 'What would you like to scaffold today?',
   choices: [
     {
-      title: "ReactJs - initialize project",
-      description: "custom project template",
+      title: 'ReactJs - initialize project',
+      description: 'custom project template',
       value: reactJsTemplate,
     },
     {
-      title: "Vite - initialize project",
-      description: "npm create vite@latest",
-      value: () => exec("npm create vite@latest"),
+      title: 'Vite - initialize project',
+      description: 'npm create vite@latest',
+      value: () => exec('npm create vite@latest'),
     },
     {
-      title: "Astro (multiple options)",
+      title: 'Astro (multiple options)',
       value: {
-        name: "choice",
-        type: "select",
-        message: "Astro options:",
+        name: 'choice',
+        type: 'select',
+        message: 'Astro options:',
         choices: [
           {
-            title: "Astro - initialize project",
-            description: "npm create astro@latest",
-            value: () => exec("npm create astro@latest"),
+            title: 'Astro - initialize project',
+            description: 'npm create astro@latest',
+            value: () => exec('npm create astro@latest'),
           },
           {
-            title: "Tailwind - add to Astro project",
-            description: "npx astro add tailwind",
-            value: () => exec("npx astro add tailwind"),
+            title: 'Tailwind - add to Astro project',
+            description: 'npx astro add tailwind',
+            value: () => exec('npx astro add tailwind'),
           },
         ],
       },
     },
     {
-      title: "Eslint - add to project",
-      description: "npm init @eslint/config",
-      value: () => exec("npm init @eslint/config"),
+      title: 'Eslint - add to project',
+      description: 'npm init @eslint/config',
+      value: () => exec('npm init @eslint/config'),
     },
     {
-      title: "NestJS - initialize project",
-      description: "npm i -g @nestjs/cli; nest new",
-      value: () => exec("npm i -g @nestjs/cli; nest new"),
+      title: 'NestJS - initialize project',
+      description: 'npm i -g @nestjs/cli; nest new',
+      value: () => exec('npm i -g @nestjs/cli; nest new'),
     },
     {
-      title: "Next - initialize project",
-      description: "npx create-next-app@latest",
-      value: () => exec("npx create-next-app@latest"),
+      title: 'Next - initialize project',
+      description: 'npx create-next-app@latest',
+      value: () => exec('npx create-next-app@latest'),
     },
     {
-      title: "OpenWebComponent - initialize project",
-      description: "npm init @open-wc",
-      value: () => exec("npm init @open-wc"),
+      title: 'OpenWebComponent - initialize project',
+      description: 'npm init @open-wc',
+      value: () => exec('npm init @open-wc'),
     },
   ],
 };
 
-runPrompts(cfg);
-
-export async function runPrompts(prompt) {
-  const res = await prompts(prompt);
-  if (typeof res.choice === "object") runPrompts(res.choice);
-  else if (typeof res.choice === "function") res.choice();
+async function runPrompts(promptConfig) {
+  const res = await prompts(promptConfig);
+  if (typeof res.choice === 'object') runPrompts(res.choice);
+  else if (typeof res.choice === 'function') res.choice();
 }
+
+runPrompts(cfg);
